6160943b8b78bc6582b62f4bbb75c7ad8dc92707
edin <vlfdus0401@khu.ac.kr>
2018-10-12 10:09:43 +0900

    add multiplication table

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+  int num;
+  while(1){
+    cout<<"******구구단 출력 프로그램******"<<endl;
+    cout<<"몇 단?: (종료시 0을 누르시오)";
+    cin>>num;
+    if (num!=0)
+    {
+      for (int i=1;i<=9;i++)
+      {
+      cout<<num<<"*"<<i<<"="<<num*i<<endl;
+      }
+    }
+    else exit(1);
+  }
+  return 0;
+}
